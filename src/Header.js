d19842073296d1a15a96ed5ee647e4ea2780243a
Ganzolboo Ayurzana <a.ganzolboo@yahoo.com>
2018-11-29 21:29:47 -0800

    Lab

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1> My App</ h1>
+                <h1> My App, Ganzolboo</ h1>
                 <nav>
                     <ul>
                         <li>
